344c7c03adcb2552f54f971ed076685a37b5a7fd
John Scalo <scalo@madebywindmill.com>
2021-02-03 21:12:54 -0600

    MBWAppKitExtras

---
+++ Sources/MBWUtils/MBWAppKitExtras.swift
@@ -0,0 +1,45 @@
+//
+//  MBWAppKitExtras.swift
+//
+//  Created by John Scalo on 2/3/21.
+//  Copyright © 2018-2021 Made by Windmill. All rights reserved.
+//
+
+#if os(OSX)
+
+import AppKit
+
+extension NSImage {
+        
+    func resize(targetSizePx: CGSize) -> NSImage {
+        let size = self.size
+        
+        let widthRatio  = targetSizePx.width  / self.size.width
+        let heightRatio = targetSizePx.height / self.size.height
+        var newSize: CGSize
+        
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: floor(size.width * heightRatio), height: floor(size.height * heightRatio))
+        } else {
+            newSize = CGSize(width: floor(size.width * widthRatio),  height: floor(size.height * widthRatio))
+        }
+        
+        return self.resize(newSize)
+    }
+    
+    func resize(_ newSize: NSSize) -> NSImage {
+        let newImage = NSImage(size: newSize)
+        newImage.lockFocus()
+        NSGraphicsContext.current?.imageInterpolation = .high
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height),
+                  from: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height),
+                  operation: .sourceOver,
+                  fraction: 1.0)
+        newImage.unlockFocus()
+        return newImage
+    }
+    
+}
+
+
+#endif
